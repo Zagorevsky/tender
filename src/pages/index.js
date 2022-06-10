554b53159cd652456bc7d56795c4bf8653bf30b3
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-06-10 18:47:18 +0300

    feat: Popup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
-const usdCbRf = document.querySelector('.sidenav__cbr_usd');
+const usdCbRf = document.querySelector('.header__cbr-usd');
+const buttonSignin = document.querySelector('.header__button');
+const popupSignin = document.querySelector('.popup__signin');
+const buttonCancelPopup = popupSignin.querySelector('.popup__close');
+const nameUser = document.querySelector('.header__name-user');
+const nameUserInput = document.querySelector('.popup__input-name');
 
 getUsdCbRf = () => {
-
   fetch('https://iss.moex.com/iss/statistics/engines/currency/markets/selt/rates.json?iss.meta=off')
     .then((response) => {
       if (!response.ok) {
@@ -17,6 +21,35 @@ getUsdCbRf = () => {
     });
 }
 
+function setEventListener() {
+  buttonSignin.addEventListener('click', openPopupSignin);
+  buttonCancelPopup.addEventListener('click', closePopupSignin);
+  popupSignin.querySelector('.popup__form').addEventListener('submit', sendFormSignin);
+}
+
+function openPopup (namePopup) {
+  namePopup.querySelector('.popup__form').reset();
+  namePopup.classList.add('popup_opened');
+}
+
+function closePopup (namePopup) {
+  namePopup.classList.remove('popup_opened');
+}
+
+function openPopupSignin() {
+  openPopup(popupSignin)
+}
+
+function closePopupSignin() {
+  closePopup(popupSignin)
+}
+
+function sendFormSignin(event) {
+  event.preventDefault();
+  console.log(nameUserInput.value)
+  nameUser.textContent = nameUserInput.value;
+  closePopupSignin()
+}
+
 getUsdCbRf();
-
-
+setEventListener();
